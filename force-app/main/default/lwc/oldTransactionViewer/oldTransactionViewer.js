9dbdc21e17f9b9357d0b5d25ac91f5f5498b748f
Nithya Narayanan <NithyaNarayananCVR@gmail.com>
2025-09-25 12:18:41 +0530

    Enhancement : Old Transaction Record visibility via LWC from Big Objects

--- force-app/main/default/lwc/oldTransactionViewer/oldTransactionViewer.js
+++ force-app/main/default/lwc/oldTransactionViewer/oldTransactionViewer.js
@@ -6,6 +6,10 @@ import getBigRecordList from '@salesforce/apex/BigObjectController.getTransactio
     { label: 'Transaction Date', fieldName: 'Transaction_Date__c'},
     { label: 'Amount', fieldName: 'Amount__c'},
     { label: 'UPI ID', fieldName: 'UPI_ID__c' },
+    { label: 'Name', fieldName: 'Name__c'},
+    { label: 'Created Date', fieldName: 'Created_DateTime__c'},
+    { label: 'Type', fieldName: 'Type__c'},
+    { label: 'Mode', fieldName: 'Mode__C'},
     { label: 'Transaction ID', fieldName: 'Transaction__c'},
 ];
 export default class oldTransactionViewer extends LightningElement {
@@ -13,9 +17,7 @@ export default class oldTransactionViewer extends LightningElement {
   @api recordId;
   @wire(getBigRecordList,{contactId :'$recordId'})
   bigRecords;
-sampleData =[{"Id":"1234", "Contact__c":"Test contact","Amount__c":"12345.2142","Transaction_Date__c":"Date","UPI_ID__c":"UPI ID@upi","Transaction__c":"TxnId"}];
   get hasData(){
     return this.bigRecords.data;
   }
-
 }
