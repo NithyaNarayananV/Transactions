29380f902c584cc9fa5c159776a067a29550f81b
Nithya Narayanan <NithyaNarayananCVR@gmail.com>
2025-09-25 09:58:59 +0530

    Updating Apex for Bigobject

--- force-app/main/default/lwc/oldTransactionViewer/oldTransactionViewer.js
+++ force-app/main/default/lwc/oldTransactionViewer/oldTransactionViewer.js
@@ -2,13 +2,14 @@ import {LightningElement, wire, api} from 'lwc';
 import getBigRecordList from '@salesforce/apex/BigObjectController.getTransactionBigDataRecordsforContact';
   const columns = [
     { label: 'Id', fieldName: 'Id' },
-    { label: 'Contact Id', fieldName: 'Contact__c'},
-    { label: 'Amount', fieldName: 'Amount__c'},
+    //{ label: 'Contact Id', fieldName: 'Contact__c'},
     { label: 'Transaction Date', fieldName: 'Transaction_Date__c'},
+    { label: 'Amount', fieldName: 'Amount__c'},
     { label: 'UPI ID', fieldName: 'UPI_ID__c' },
     { label: 'Transaction ID', fieldName: 'Transaction__c'},
 ];
 export default class oldTransactionViewer extends LightningElement {
+  columns = columns;
   @api recordId;
   @wire(getBigRecordList,{contactId :'$recordId'})
   bigRecords;
